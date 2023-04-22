0b67809b74cae4f98f0ae570b9a1f0774c15c654
yhibo <yhiboblado@gmail.com>
2023-04-22 01:41:08 -0300

    Added level 5

--- Level_3/fuel_injection_perfection/test_solution.py
+++ Level_3/fuel_injection_perfection/test_solution.py
@@ -4,7 +4,7 @@ from solution import solution
 
 def test_solution():
     """Test cases for solution.py"""
-    assert solution("4") == 2
+    assert  solution("4") == 2
     assert solution("15") == 5
     assert solution("1") == 0
     assert solution(str(2**100 - 1)) == 101
